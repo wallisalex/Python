ea9b1374d650b93f7726b7d30c50bfd1360467cc
alex <Alexwallis443@gmail.com>
2021-07-18 13:38:22 -0400

    added to project

--- Project 01.py
+++ Project 01.py
@@ -8,33 +8,27 @@ def main():
     b = 1 
     c = 2
     
-    def getGuess(): #function with return
-        clear = ('\n' * 40)
-        print(clear)
+    print("+--------------------------------------+")
+    print( "|        WELCOME TO PICA-CENTRO        |")
+    print ("+--------------------------------------+")
 
-
-
-    def PicaCentro(): #setting up game function
-        print ("+--------------------------------------+")
-        print( "|        WELCOME TO PICA-CENTRO        |")
-        print ("+--------------------------------------+")
-
-        a = int(input("Please enter the first digit in your number : "))
-        b = int(input("Please enter the seccond digit in your number : "))
-        c = int(input("Please enter the third digit in your number : "))
-
-
-        def SecretNums(a,b,c): #function with multiple returns
-           print()
-
-            
+    a = int(input("Please enter the first digit in your number : "))
+    b = int(input("Please enter the seccond digit in your number : "))
+    c = int(input("Please enter the third digit in your number : "))
+       
+            #Digits in guess, every digit can be a pica,centro,neither BUT CANT BE BOTH
+            #repeat digits
+            #ElIf 
 
         
-        print("The secret number has been set to: " + 4)
+    print("The secret number has been set to: " + 4)
 
-
-
-
-    PicaCentro()
 
 main()
+
+def getGuess(): #function with return
+    clear = ('\n' * 40)
+    print(clear)
+
+def SecretNums(a,b,c): #function with multiple returns
+    print()
