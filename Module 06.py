57ee611baafda24b4f94ceb0f65a4f08903e114e
alex <Alexwallis443@gmail.com>
2021-07-21 16:15:05 -0400

    program is finally writing to file

--- Module 06.py
+++ Module 06.py
@@ -1,8 +1,7 @@
-# Author Alexander Wallis
-# Written for Python 3.9.6
+#Author Alexander Wallis
+#Written for Python 3.9.6
 
 # IMPORTS
-
 import random
 
 # GLOBALS
@@ -15,41 +14,67 @@ UPPER_BOUND = 10000 # the highest number that can go in the data file
 
 NUM_ELEMENTS = 1000
 
+# FUNCTION DEFINITIONS
 def main():
+    # create a file with a random number of random integers
+    
+
+    # read the numbers from the file with a while loop
+    ReadFileWithWhile()
+
+
 
     # generate the random number file
 
     GenerateRandomNumberFile(NUM_ELEMENTS)
-    test()
 
     # determine the average value in the file
 
-    elementCount, averageValue = DetermineAverageValue(FILE_NAME)
+     #elementCount, averageValue = DetermineAverageValue(FILE_NAME)
 
     # display the results
+   
 
-    print ('There were %d elements found in the file.') % (elementCount)
+    #print ('There were %d elements found in the file.') % (elementCount)
 
-    print ('The average value of the elements was determined to be %.3f.') % (averageValue)
+    #print ('The average value of the elements was determined to be %.3f.') % (averageValue)
+
+
 
 def GenerateRandomNumberFile(numberOfElements):
+    x = numberOfElements
 
-    outHandle = open('A06S-DataFile.dat', 'w')
+    # open the file for writing
+    outHandle = open(FILE_NAME, 'w')
 
-    rand = random.rand(LOWER_BOUND,UPPER_BOUND)
-    print(rand)
+    # generate random numbers and write them to the file
+    for x in range(1, random.randrange(1, x + 1)):
+        outHandle.write(str(random.randrange(LOWER_BOUND, UPPER_BOUND)) + '\n')
 
+    # close the file
     outHandle.close()
 
-def DetermineAverageValue(fileName):
 
-    # IMPLEMENT ME! 
-     print
+def ReadFileWithWhile():
+    # open the file for reading
+    inHandle = open(FILE_NAME, 'r')
 
-def test():
-    inHandle = open('A06S-DataFile.dat', 'r')
-    # READ all of the data from the file and store it in a buffer.
-    buffer = inHandle.read()
+    # read the numbers from the file with a while loop
+    # priming read
+    temp = inHandle.readline()
+    temp = temp.rstrip('\n')
+    while temp != '':
+        # display the number that was read
+        print (temp, 'was read from the file.')
 
-    # display the file contents on the screen
-    print (buffer)
+        # read the next number.
+        # Note: After the last number in the file has been read, the
+        #    readline method will return an empty string.  This value
+        #    will be used to detect the end of the input file.
+        temp = inHandle.readline()
+        temp = temp.rstrip('\n')
+
+
+
+# Begin MAIN()
+main()
