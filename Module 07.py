09dae021c76c15f61844ac6f2df78f0f363c6d0c
alex <Alexwallis443@gmail.com>
2021-07-21 18:45:58 -0400

    module is a big challenge

--- Module 07.py
+++ Module 07.py
@@ -1,28 +1,13 @@
 
+
+# GLOBALS
+
+FILE_NAME = 'A07S-Genesis.txt'
+
+
 def main():
-    
-# make a string that contains multiple words
+       
+     
 
-    manyWordString = "This is a string that has many words in it."
-
-    print ("Before Splitting:")
-
-    print (manyWordString)
-
-    print ("")
-
-# split the string into a list of words
-
-    wordList = manyWordString.split()
-
-# iterate through the wordList with an index to
-
-# visualize what the split function did
-
-    print ("After Splitting:")
-
-    for index in range( len(wordList) ):
-
-        print ("wordList[%2d] contains: %s" % (index, wordList[index]))
 
 main()
--- Test Enviorment.py
+++ Test Enviorment.py
@@ -1,66 +1,27 @@
-# Author: Mark Dencler
-# Description: This program demonstrates techniques used to read data
-#    from arbitrary sized files.
-
-# IMPORTS
-import random
-
-# GLOBALS
-FILE_NAME = 'W06-05-DataFile.txt'
-ELEMENT_CAP = 1000
-
-# FUNCTION DEFINITIONS
 def main():
-    # create a file with a random number of random integers
-    GenerateRandomNumbersFile()
-
-    # read the numbers from the file with a while loop
-    #ReadFileWithWhile()
-
-    # read the numbers from the file with a for loop
-    ReadFileWithFor()
-
-
-def GenerateRandomNumbersFile():
-    # open the file for writing
-    outHandle = open(FILE_NAME, 'w')
-
-    # generate random numbers and write them to the file
-    for x in range(1, random.randrange(1, ELEMENT_CAP + 1)):
-        outHandle.write(str(random.randrange(1, 10001)) + '\n')
-
-    # close the file
-    outHandle.close()
-
-
-def ReadFileWithWhile():
-    # open the file for reading
-    inHandle = open(FILE_NAME, 'r')
-
-    # read the numbers from the file with a while loop
-    # priming read
-    temp = inHandle.readline()
-    temp = temp.rstrip('\n')
-    while temp != '':
-        # display the number that was read
-        print (temp, 'was read from the file.')
-
-        # read the next number.
-        # Note: After the last number in the file has been read, the
-        #    readline method will return an empty string.  This value
-        #    will be used to detect the end of the input file.
-        temp = inHandle.readline()
-        temp = temp.rstrip('\n')
-
-def ReadFileWithFor():
-    # open the file for reading
-    inHandle = open(FILE_NAME, 'r')
-
-    # read the numbers from the file with a for loop
-    for temp in inHandle:
-        temp = temp.rstrip('\n')
-        print (temp, 'was read from the file.')
     
+# make a string that contains multiple words
 
-# Begin MAIN()
+    manyWordString = "This is a string that has many words in it."
+
+    print ("Before Splitting:")
+
+    print (manyWordString)
+
+    print ("")
+
+# split the string into a list of words
+
+    wordList = manyWordString.split()
+
+# iterate through the wordList with an index to
+
+# visualize what the split function did
+
+    print ("After Splitting:")
+
+    for index in range( len(wordList) ):
+
+        print ("wordList[%2d] contains: %s" % (index, wordList[index]))
+
 main()
