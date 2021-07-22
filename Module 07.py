1ccd30d66799594054137e93aeb8fce815a414d1
alex <Alexwallis443@gmail.com>
2021-07-21 20:22:32 -0400

    a list of unique values is difficult

--- Module 07.py
+++ Module 07.py
@@ -10,22 +10,27 @@ def main():
        
                         
    with open(FILE_NAME, "r") as new: # opens the file and names the list my_file then splits each line into singular strings
-        
-           
+
         temp = new.readline() #setting up read file 
         temp = temp.rstrip('\n')
 
 
         while temp != '': #while that keeps repeating if there is not a space
-            temp = new.readline()
-            str = temp.split()
+            temp = new.readline() #to make sure the loop goes through the entire list and doesnt print the same line endlessly 
+            TheList = temp.split()
             
-            for index in range(len(str) ):
-                if (str[index].isalpha()):
-                    str[index] = (str[index].upper())
-                    print (str[index])
-                
-
-
-
+            for index in range(len(TheList) ):
+                if (TheList[index].isalpha()): #checking the list for alphanumeric characters and avoiding numbers
+                    TheList[index] = (TheList[index].upper()) #converting strings in list to UPPERCASE letters
+                    TheList.sort()
+                    
+                    
+                    #myset = set(TheList[index]) # Atempted making a set, couldnt figure it out 
+                    #mynewlist = list(myset)
+             
+                    
+        
+                    
+                    
+                    
 main()
