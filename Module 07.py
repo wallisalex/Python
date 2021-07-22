6501f4002cc8cefc0c4b131bcac3970ceaff7e37
alex <Alexwallis443@gmail.com>
2021-07-22 00:03:00 -0400

    its kind of working now

--- Module 07.py
+++ Module 07.py
@@ -15,13 +15,16 @@ def main():
             #https://www.w3schools.com/python/python_lists_comprehension.asp 
             listli = [item for item in listli if item.isalpha()] #Return's characters in the string are alphabetic
             listli = [each_string.upper() for each_string in listli] #uses list comprehension to create a new list which coverts all letters to uppercase
+            #listli = [each_string.split() for each_string in listli] 
+            #Attempting to remove duplicates with comprehension
+            
+            [listli.append(b) for b in listli if b not in listli]  #https://www.guru99.com/python-howto-remove-duplicates.html
 
-
+            
             m = listl.append(listli)
         
-
-    for index in range( len(listl) ):
-        print (listl[index])
+    listli.sort()
+    print(listli)
         
         
     
