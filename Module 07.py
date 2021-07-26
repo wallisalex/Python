8e64cec2c267876a6cce0c479ab594a7899d55c6
alex <Alexwallis443@gmail.com>
2021-07-25 22:15:34 -0400

    Module 07 makes sense now

--- Module 07.py
+++ Module 07.py
@@ -1,39 +1,64 @@
 
 
 # GLOBALS
-FILE_NAME = 'A07S-Genesis.txt'
-TEST_FILE = 'OUTPUT.txt'
+INPUT_FILE = 'A07S-Genesis.txt'
+OUTPUT_FILE = 'A07S-Genesis-Sorted.txt'
 
 
 def main():
+
+    #creates list to fill with strings found in the file 
+    listOfWords = []
+
+    inHandle = open(INPUT_FILE, "r") #opening the file for read 
+
+    lineBuffer = inHandle.readline() #reading the first line in the file 
     
-    with open(FILE_NAME,'r') as f:
-        listl = [] #creates list to fill with strings
-        for line in f: #for loops strips down the string and breaks it into seperate strings #!!! PROBLEM HERE??
-            strip_lines = line.strip()
-            listli = strip_lines.split()
-            #print(listli)
-            #https://www.w3schools.com/python/python_lists_comprehension.asp 
-            listli = [item for item in listli if item.isalpha()] #Return's characters in the string are alphabetic
-            listli = [each_string.upper() for each_string in listli] #uses list comprehension to create a new list which coverts all letters to uppercase
-            #listli = [each_string.split() for each_string in listli] 
-            #Attempting to remove duplicates with comprehension
-            listli.sort()
-            my_finallist = []
-            [my_finallist.append(n) for n in listli if n not in my_finallist]  #https://www.guru99.com/python-howto-remove-duplicates.html
-
-            
-            #m = my_finallist.append(my_finallist)
-        f.close()
-
-    #for index in range(len(my_finallist) ):
+    #using the idea from Module 06 to read through all the lines of the file
+    while lineBuffer != "":
+        splitStrings = lineBuffer.split()  # split the strings located in the line inputed 
         
-       # print (my_finallist[index])
+        #splits the string in seperate strings 
+        for index in range( len(splitStrings) ):
+            splitStrings[index] = exportWord(splitStrings[index])
+           
 
-    new = open(TEST_FILE, 'w')    
-    new.write(str(my_finallist))
+            if splitStrings[index] not in listOfWords: #determines if the string is already in the sortedList, if not add it 
+                if splitStrings[index] != "": #if the string is also not a blank, then append the postion and add the new string
+                    listOfWords.append(splitStrings[index])
 
-        # close the file
-    new.close()
+        #reads in the next line
+        lineBuffer = inHandle.readline() 
 
+    inHandle.close() #stops the reading of the file
+
+    listOfWords.sort() #sorts the unsorted list from A-Z                    listOfWords = listOfWords.sort() cause the program to crash
+    
+    outHandle = open(OUTPUT_FILE, 'w') #Creating a new file for the sorted words to write to
+
+    #takes the words from the sorted list and writes them to the file
+
+    for element in listOfWords:
+        outHandle.write(element + '\n') #takes the elements in the list and prints them line by line and creates a new line per element
+
+    outHandle.close() #stops writing to the file
+
+#creating functions to do the hard work for me 
+
+def exportWord(inputString):
+    
+    if str(inputString).isalpha(): #if the input string contains numbers leave it be 
+        inputString = str(inputString).upper()   #makes all the letters in the string uppercase #added the (str) to fix the error 
+
+        exportString = inputString #sets the export string equal the the 
+
+        return str(exportString) #ensures the exportString is actually a string when returning
+
+    else:   
+        return "" #return a blank which will not be added
+
+        
 main()
+
+
+    
